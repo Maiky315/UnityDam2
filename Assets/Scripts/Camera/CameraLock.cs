42519499ac5ab3757fead0b5488f1273601e6d33
Mikel Garcia <88591871+Maiky315@users.noreply.github.com>
2023-01-18 13:29:42 +0100

    camara y movimientos

--- Assets/Scripts/Camera/CameraLock.cs
+++ Assets/Scripts/Camera/CameraLock.cs
@@ -27,7 +27,7 @@ public class CameraLock : MonoBehaviour
 
         xRotation -= mouseY;
 
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation = Mathf.Clamp(xRotation, 0f, 0f);
 
         transform.localRotation = Quaternion.Euler(xRotation,0,0);
         playerBody.Rotate(Vector3.up * mouseX);
---
+++ Assets/Scripts/Player/PlayerMover.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+
+    public CharacterController characterController;
+
+    public float speed = 10f;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        characterController.Move(move * speed * Time.deltaTime);
+    }
+}

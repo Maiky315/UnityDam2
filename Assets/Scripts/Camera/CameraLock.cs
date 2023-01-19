10b11ceee05bcc5110a9afa16ca1a7e8039aa157
Mikel Garcia <88591871+Maiky315@users.noreply.github.com>
2023-01-19 11:53:15 +0100

    lll

--- Assets/Scripts/Camera/CameraLock.cs
+++ Assets/Scripts/Camera/CameraLock.cs
@@ -27,7 +27,7 @@ public class CameraLock : MonoBehaviour
 
         xRotation -= mouseY;
 
-        xRotation = Mathf.Clamp(xRotation, 0f, 0f);
+        xRotation = Mathf.Clamp(xRotation, 10f, 10f);
 
         transform.localRotation = Quaternion.Euler(xRotation,0,0);
         playerBody.Rotate(Vector3.up * mouseX);
